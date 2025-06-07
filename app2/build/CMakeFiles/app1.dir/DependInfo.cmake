5b22633320f5dc6593b89a756b84f729a650145e
RazvanCiobanu755 <razvan.ciobanu06@e-uvt.ro>
2025-06-07 11:43:22 +0300

    Add files via upload

---
+++ app2/build/CMakeFiles/app1.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/razvan/Desktop/proiectp/console.cpp" "CMakeFiles/app1.dir/console.cpp.o" "gcc" "CMakeFiles/app1.dir/console.cpp.o.d"
+  "/home/razvan/Desktop/proiectp/game.cpp" "CMakeFiles/app1.dir/game.cpp.o" "gcc" "CMakeFiles/app1.dir/game.cpp.o.d"
+  "/home/razvan/Desktop/proiectp/main.cpp" "CMakeFiles/app1.dir/main.cpp.o" "gcc" "CMakeFiles/app1.dir/main.cpp.o.d"
+  "/home/razvan/Desktop/proiectp/user.cpp" "CMakeFiles/app1.dir/user.cpp.o" "gcc" "CMakeFiles/app1.dir/user.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ app2/build/CMakeFiles/app1.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app1.dir/console.cpp.o"
+  "CMakeFiles/app1.dir/console.cpp.o.d"
+  "CMakeFiles/app1.dir/game.cpp.o"
+  "CMakeFiles/app1.dir/game.cpp.o.d"
+  "CMakeFiles/app1.dir/main.cpp.o"
+  "CMakeFiles/app1.dir/main.cpp.o.d"
+  "CMakeFiles/app1.dir/user.cpp.o"
+  "CMakeFiles/app1.dir/user.cpp.o.d"
+  "app1"
+  "app1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
